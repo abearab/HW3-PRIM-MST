2c6fc8df70da789d637503bc3bd7023373ce56d3
Abe Arab <abarbiology@gmail.com>
2025-01-29 23:48:01 -0800

    update course information and version number in __init__.py

--- mst/__init__.py
+++ mst/__init__.py
@@ -1,8 +1,9 @@
 """
-BMI 203: Biocomputing Algorithms - Winter 2023
+BMI 203: Biocomputing Algorithms - Winter 2025
 HW 4: Prim's Algorithm
+Student: Abe Arab
 """
 
 from .graph import Graph
 
-__version__ = '0.1.0'
+__version__ = '0.2.5'
